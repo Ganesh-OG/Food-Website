6b1191869aff9c69ae3bf9da827079f615dda2a0
Ganesh-OG <164323965+Ganesh-OG@users.noreply.github.com>
2026-05-10 08:23:04 +0530

    Add GitHub configuration in config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+const GITHUB_CONFIG = {
+
+    TOKEN: "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",
+
+    USERNAME: "Ganesh-OG",
+
+    REPO: "Food-Website",
+
+    FOLDER_PATH: "Images/logo"
+};
